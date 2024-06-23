74a9ba55a4f359518fb04e6fe29ecaf45914ee9e
Khushi Chauhan <ksingh77038@gmail.com>
2024-06-24 01:20:33 +0530

    datatype comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,26 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 === 1);
+// console.log(2 != 1);
+// console.log(2 !== 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); // true, which is not a predictable result
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+// we avoid conversions like this (null and undefined)
+
+// === (checks valye strictly and datatype too)
+
+ console.log("2" == 2);
+ console.log("2" === 2);
