2a2710f0657fdcfc529e19480fb8a31384ee68d4
Khushi Chauhan <ksingh77038@gmail.com>
2024-06-24 00:59:56 +0530

    conversions and operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -37,3 +37,46 @@ let someNumber = 33
 let stringNumber = String(someNumber);
 console.log(someNumber);
 console.log(typeof stringNumber);
+
+// ********************* Operations ********************* 
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3); // 2 raise to the power 2
+console.log(2/3);
+console.log(2%3); // mudulud/remainder
+
+let str1 = "Khushi"
+let str2 = "Rajput"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2"); //ToPrimitive (Inputs and preferredType)
+
+console.log((3 + 4) * 5 % 3);
+
+console.log(true);
+console.log(+true); // true+ is not possible
+console.log(+"");
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++; // prefix and postfix Operators
+console.log(gameCounter);
+
+// link: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
