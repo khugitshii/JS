c4c907375993e3ab21b5784d1af17aaac84e6ccd
Khushi Chauhan <ksingh77038@gmail.com>
2024-07-08 18:31:50 +0530

    switch case and control flow

---
+++ 03_basics/07_switch.js
@@ -0,0 +1,87 @@
+// Switch Case
+// syntax:
+
+// switch (key) {
+//     case value:
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+// // if else will be time-taking, so, we will use switch cases
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+
+//     case 2:
+//         console.log("February");
+//         break;
+
+//     case 3:
+//         console.log("March");
+//         break;
+
+//     case 4:
+//         console.log("April");
+//          break;
+
+//     case 5:
+//         console.log("May");
+//         break;
+
+//     default:
+//         console.log("default case match");
+//         break;
+// }
+
+// or string
+
+const month = "April"
+// if else will be time-taking, so, we will use switch cases
+switch (month) {
+    case "January":
+        console.log("January");
+        break;
+
+    case "February":
+        console.log("February");
+        break;
+
+    case "March":
+        console.log("March");
+        break;
+
+    case "April":
+        console.log("April");
+         break;
+
+    case "May":
+        console.log("May");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
